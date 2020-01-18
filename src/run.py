126294cdb0caf86841bfc0fc0200d2ce039819ed
Alun Stokes <stokea1@mcmaster.ca>
2020-01-18 14:47:28 -0500

    Upload embeddings

--- src/run.py
+++ src/run.py
@@ -1,5 +1,23 @@
 import pickle
+import tensorflow as tf
+import tensorflow_hub as hub
+
 
 data = pickle.load(open( "../processed/courses.p", "rb" ))
 
-print(len(data))
+desc = []
+i = 0
+while i < len(data):
+    desc.append(data[i]['desc'])
+    i += 1
+
+module_url = "https://tfhub.dev/google/nnlm-en-dim128/2"
+embed = hub.KerasLayer(module_url)
+embeddings = embed(desc)
+
+i = 0
+while i < len(data):
+    data[i]['embedding'] = embeddings[i]
+    i += 1
+
+pickle.dump(data, open( "../processed/courses.p", "wb" ))
